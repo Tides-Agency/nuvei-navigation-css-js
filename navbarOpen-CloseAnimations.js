adf3b44f53d75fe287ca34e181296ef7a92df453
dashaShuhlia <156914541+dashaShuhlia@users.noreply.github.com>
2024-07-17 19:09:03 +0200

    Add files via upload

---
+++ navbarOpen-CloseAnimations.js
@@ -0,0 +1,190 @@
+const animations = {
+    "a-130": {
+        "id": "a-130",
+        "title": "Navbar menu [Open] 6",
+        "actionItemGroups": [{
+            "actionItems": [{
+                "id": "a-130-n",
+                "actionTypeId": "STYLE_SIZE",
+                "config": {
+                    "delay": 0,
+                    "easing": "inOutQuint",
+                    "duration": 200,
+                    "target": {
+                        "useEventTarget": "CHILDREN",
+                        "selector": ".menu-icon_line-middle",
+                        "selectorGuids": ["2b10253a-34e5-4794-622f-053421329498"]
+                    },
+                    "widthValue": 0,
+                    "widthUnit": "px",
+                    "heightUnit": "PX",
+                    "locked": false
+                }
+            }, {
+                "id": "a-130-n-2",
+                "actionTypeId": "TRANSFORM_MOVE",
+                "config": {
+                    "delay": 0,
+                    "easing": "inOutQuint",
+                    "duration": 400,
+                    "target": {
+                        "useEventTarget": "CHILDREN",
+                        "selector": ".menu-icon_line-bottom",
+                        "selectorGuids": ["2b10253a-34e5-4794-622f-05342132949a"]
+                    },
+                    "yValue": -8,
+                    "xUnit": "PX",
+                    "yUnit": "px",
+                    "zUnit": "PX"
+                }
+            }, {
+                "id": "a-130-n-3",
+                "actionTypeId": "TRANSFORM_MOVE",
+                "config": {
+                    "delay": 0,
+                    "easing": "inOutQuint",
+                    "duration": 400,
+                    "target": {
+                        "useEventTarget": "CHILDREN",
+                        "selector": ".menu-icon_line-top",
+                        "selectorGuids": ["2b10253a-34e5-4794-622f-053421329497"]
+                    },
+                    "yValue": 8,
+                    "xUnit": "PX",
+                    "yUnit": "px",
+                    "zUnit": "PX"
+                }
+            }, {
+                "id": "a-130-n-4",
+                "actionTypeId": "TRANSFORM_ROTATE",
+                "config": {
+                    "delay": 0,
+                    "easing": "inOutQuint",
+                    "duration": 600,
+                    "target": {
+                        "useEventTarget": "CHILDREN",
+                        "selector": ".menu-icon_line-top",
+                        "selectorGuids": ["2b10253a-34e5-4794-622f-053421329497"]
+                    },
+                    "zValue": -45,
+                    "xUnit": "DEG",
+                    "yUnit": "DEG",
+                    "zUnit": "deg"
+                }
+            }, {
+                "id": "a-130-n-5",
+                "actionTypeId": "TRANSFORM_ROTATE",
+                "config": {
+                    "delay": 0,
+                    "easing": "inOutQuint",
+                    "duration": 600,
+                    "target": {
+                        "useEventTarget": "CHILDREN",
+                        "selector": ".menu-icon_line-bottom",
+                        "selectorGuids": ["2b10253a-34e5-4794-622f-05342132949a"]
+                    },
+                    "zValue": 45,
+                    "xUnit": "DEG",
+                    "yUnit": "DEG",
+                    "zUnit": "deg"
+                }
+            }]
+        }],
+        "useFirstGroupAsInitialState": false,
+        "createdOn": 1626168378054
+    },
+    "a-131": {
+        "id": "a-131",
+        "title": "Navbar menu [Close] 6",
+        "actionItemGroups": [{
+            "actionItems": [{
+                "id": "a-131-n",
+                "actionTypeId": "TRANSFORM_MOVE",
+                "config": {
+                    "delay": 0,
+                    "easing": "inOutQuint",
+                    "duration": 600,
+                    "target": {
+                        "useEventTarget": "CHILDREN",
+                        "selector": ".menu-icon_line-bottom",
+                        "selectorGuids": ["2b10253a-34e5-4794-622f-05342132949a"]
+                    },
+                    "yValue": 0,
+                    "xUnit": "PX",
+                    "yUnit": "px",
+                    "zUnit": "PX"
+                }
+            }, {
+                "id": "a-131-n-2",
+                "actionTypeId": "TRANSFORM_MOVE",
+                "config": {
+                    "delay": 0,
+                    "easing": "inOutQuint",
+                    "duration": 600,
+                    "target": {
+                        "useEventTarget": "CHILDREN",
+                        "selector": ".menu-icon_line-top",
+                        "selectorGuids": ["2b10253a-34e5-4794-622f-053421329497"]
+                    },
+                    "yValue": 0,
+                    "xUnit": "PX",
+                    "yUnit": "px",
+                    "zUnit": "PX"
+                }
+            }, {
+                "id": "a-131-n-3",
+                "actionTypeId": "TRANSFORM_ROTATE",
+                "config": {
+                    "delay": 0,
+                    "easing": "inOutQuint",
+                    "duration": 400,
+                    "target": {
+                        "useEventTarget": "CHILDREN",
+                        "selector": ".menu-icon_line-bottom",
+                        "selectorGuids": ["2b10253a-34e5-4794-622f-05342132949a"]
+                    },
+                    "zValue": 0,
+                    "xUnit": "DEG",
+                    "yUnit": "DEG",
+                    "zUnit": "deg"
+                }
+            }, {
+                "id": "a-131-n-4",
+                "actionTypeId": "TRANSFORM_ROTATE",
+                "config": {
+                    "delay": 0,
+                    "easing": "inOutQuint",
+                    "duration": 400,
+                    "target": {
+                        "useEventTarget": "CHILDREN",
+                        "selector": ".menu-icon_line-top",
+                        "selectorGuids": ["2b10253a-34e5-4794-622f-053421329497"]
+                    },
+                    "zValue": 0,
+                    "xUnit": "DEG",
+                    "yUnit": "DEG",
+                    "zUnit": "deg"
+                }
+            }, {
+                "id": "a-131-n-5",
+                "actionTypeId": "STYLE_SIZE",
+                "config": {
+                    "delay": 400,
+                    "easing": "inOutQuint",
+                    "duration": 200,
+                    "target": {
+                        "useEventTarget": "CHILDREN",
+                        "selector": ".menu-icon_line-middle",
+                        "selectorGuids": ["2b10253a-34e5-4794-622f-053421329498"]
+                    },
+                    "widthValue": 24,
+                    "widthUnit": "px",
+                    "heightUnit": "PX",
+                    "locked": false
+                }
+            }]
+        }],
+        "useFirstGroupAsInitialState": false,
+        "createdOn": 1626168766736
+    }
+};
